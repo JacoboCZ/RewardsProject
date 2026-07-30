82fc425af4cd577eee189bece5df99fc1662bf29
Blancobo <a21310362@ceti.mx>
2026-07-30 13:48:12 -0600

    Commiteada con randomizer en búsquedas

--- reward.py
+++ reward.py
@@ -12,6 +12,8 @@ with csv_path.open("r", encoding="utf-8-sig", newline="") as archivo_csv:
     next(lector, None)  # omite la fila de encabezado
     palabras = [fila[1] for fila in lector if len(fila) > 1 and fila[1].strip()]
 
+random.shuffle(palabras)
+
 time.sleep(2)  # Espera 2 segundos antes de iniciar la acción
 pyautogui.click(887, 40)  # Simula un clic del mouse en la posición actual
 
@@ -24,6 +26,6 @@ for palabra in palabras:
 
     pyautogui.click(887, 40)  # vuelve a enfocar la ventana de texto
     pyautogui.press('enter')  # presiona enter después de cada palabra
-    time.sleep(random.uniform(0.5, 10))  # pequeña pausa entre palabras
+    time.sleep(random.uniform(3, 10))  # pequeña pausa entre palabras
 
     pyautogui.hotkey('alt', 'f4')  # cierra la ventana después de cada búsqueda
