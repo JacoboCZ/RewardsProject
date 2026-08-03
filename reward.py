24ba4fdc31220e3cbe9b74ce0e9aaa594d831155
Blancobo <a21310362@ceti.mx>
2026-08-03 08:37:25 -0600

    Comiteada.exe

--- reward.py
+++ reward.py
@@ -5,27 +5,30 @@ from pathlib import Path
 
 import pyautogui
 
-csv_path = Path(__file__).resolve().parent / "recourses" / "most_streamed_spotify_2025.csv"
+csv_path = Path(__file__).resolve().parent / "resources" / "most_streamed_spotify_2025.csv"
 
-with csv_path.open("r", encoding="utf-8-sig", newline="") as archivo_csv:
-    lector = csv.reader(archivo_csv)
-    next(lector, None)  # omite la fila de encabezado
-    palabras = [fila[1] for fila in lector if len(fila) > 1 and fila[1].strip()]
+try:
+    with csv_path.open("r", encoding="utf-8-sig", newline="") as archivo_csv:
+        lector = csv.reader(archivo_csv)
+        next(lector, None)  # omite la fila de encabezado
+        palabras = [fila[1] for fila in lector if len(fila) > 1 and fila[1].strip()]
 
-random.shuffle(palabras)
+    random.shuffle(palabras)
 
-time.sleep(2)  # Espera 2 segundos antes de iniciar la acción
-pyautogui.click(887, 40)  # Simula un clic del mouse en la posición actual
+    time.sleep(2)  # Espera 2 segundos antes de iniciar la acción
+    pyautogui.click(887, 40)  # Simula un clic del mouse en la posición actual
 
-for palabra in palabras:
-    pyautogui.click(887, 40)  # vuelve a enfocar la ventana de texto antes de escribir
+    for palabra in palabras:
+        pyautogui.click(887, 40)  # vuelve a enfocar la ventana de texto antes de escribir
 
-    for letra in palabra:
-        pyautogui.write(letra)
-        time.sleep(0.3)  # pausa entre cada letra
+        for letra in palabra:
+            pyautogui.write(letra)
+            time.sleep(0.3)  # pausa entre cada letra
 
-    pyautogui.click(887, 40)  # vuelve a enfocar la ventana de texto
-    pyautogui.press('enter')  # presiona enter después de cada palabra
-    time.sleep(random.uniform(3, 10))  # pequeña pausa entre palabras
+        pyautogui.click(887, 40)  # vuelve a enfocar la ventana de texto
+        pyautogui.press('enter')  # presiona enter después de cada palabra
+        time.sleep(random.uniform(3, 6))  # pequeña pausa entre palabras
 
-    pyautogui.hotkey('alt', 'f4')  # cierra la ventana después de cada búsqueda
+        pyautogui.hotkey('alt', 'f4')  # cierra la ventana después de cada búsqueda
+except KeyboardInterrupt:
+    print("El programa ha sido interrumpido por el usuario.")
